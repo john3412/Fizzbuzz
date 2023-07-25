23c1c0399776bb112d0f0ddb2eff98b56f93cfd9
Jonathan López Ayala <112882084+john3412@users.noreply.github.com>
2023-07-25 00:17:41 -0600

    fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 function fizzBuzz(n) {
-    for (let i = 0; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
         //Compara si el residuo de 3 es igual a 0 y el residuo de 5 es igual a 0
         if (i % 3 === 0 && i % 5 === 0){
             console.log('fizzbuzz');
